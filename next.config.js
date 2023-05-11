b7970cdf725214674af18aab16b2927234555f81
m_drakbar <daffaraihan03@gmail.com>
2023-05-11 14:08:05 +0700

    refactor: test assetPerfix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: 'https://daffakbar13.github.io/kedai-fortuna',
 }
 
 module.exports = nextConfig
